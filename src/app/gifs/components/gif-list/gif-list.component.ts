7669447163814783076f4f4842b20d79faf1c39f
Omar Hernández <127421759+omar15hr@users.noreply.github.com>
2025-04-14 20:14:11 -0400

    input entre componentes

---
+++ src/app/gifs/components/gif-list/gif-list.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { GifListItemComponent } from "./gif-list-item/gif-list-item.component";
+
+@Component({
+  selector: 'gif-list',
+  imports: [GifListItemComponent],
+  templateUrl: './gif-list.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class GifListComponent {
+  gifs = input.required<string[]>();
+}
--- src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
+++ src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { GifsSideMenuComponent } from "../../components/gifs-side-menu/gifs-side-menu.component";
+import { GifsSideMenuComponent } from "../../components/side-menu/gifs-side-menu/gifs-side-menu.component";
 
 @Component({
   selector: 'dashboard-page',
